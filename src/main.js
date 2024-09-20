ade51a39710934307e41524a71ca4e80c2f97aab
Silvar <kinaho2013@gmail.com>
2024-09-20 15:22:14 +1000

    add icon js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import "./style.css";
+import loadFontAwesome from './javascripts/icons.js';
+
+loadFontAwesome();
 
 if (process.env.NODE_ENV !== 'production') {
     console.log('Looks like we are in development mode!');
